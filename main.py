bdf6db1840098abdfb2a94c036730979fb626191
feliduoc <feli.cea@duocuc.cl>
2026-06-16 12:58:21 -0400

    programa en desarrollo
    se esta elaborando las funcion de menu junto a opcion, proximamente se les agregara las funciones a las opxiones

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Bienvenido al Sistema de Tareas Personales")
+
+def mostrar_menu():
+    print("\n--- SISTEMA DE TAREAS PERSONALES ---")
+    print("1. Agregar tarea")
+    print("2. Listar tareas")
+    print("3. Marcar tarea como completada")
+    print("4. Eliminar tarea")
+    print("5. Salir del sistema")
+    print("======================================")
+
+def iniciar_programa():
+  continuar = True
+
+while continuar:
+  mostrar_menu()
+  opcion = input("eliga una opcion 1-5"\n>>)
+  
