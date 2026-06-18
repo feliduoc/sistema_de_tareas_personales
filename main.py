48641f5ed0e9cb6c0fe14a4d2f8a69536e855482
feliduoc <feli.cea@duocuc.cl>
2026-06-18 16:26:02 -0400

    opcion 3 funcional

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ print("--- Bienvenido al Sistema de Tareas Personales :D ---")
 
 lista_tareas = []
 
-# aqui cree la funcion que mostrara el menu
+# aqui cree la funcion que mostrara el menu 
 def mostrar_menu():
     print("\n--- SISTEMA DE TAREAS PERSONALES ---")
     print("1. Agregar tarea")
-    print("2. Listar tareas")
+    print("2. mostrar tareas")
     print("3. Marcar tarea como completada")
     print("4. Eliminar tarea")
     print("5. Salir del sistema")
@@ -17,7 +17,7 @@ def agregar_tarea():
     nombre_tarea = input("Ingrese el nombre de la tarea: ").strip()
     
     if nombre_tarea == "":
-        print(" Error: El nombre de la tarea no puede estar vacío")
+        print("Error: El nombre de la tarea no puede estar vacío")
         return
 
     nueva_tarea = {"nombre": nombre_tarea, "estado": "Pendiente"} 
@@ -29,32 +29,54 @@ def listar_tareas():
     print("\n--- Lista de tareas ---")
     if len(lista_tareas) == 0:
         print("No hay tareas registradas")
-        return
+        return False 
     
     for i in range(len(lista_tareas)):
         tarea = lista_tareas[i]
         print(f"{i + 1}. [{tarea['estado']}] {tarea['nombre']}")
+    return True
 
+# Jueves 18: Nueva función para actualizar el estado de las tareas a Completada
+def completar_tarea():
+    print("\n--- Marcar Tarea como Completada ---")
+
+    if len(lista_tareas) == 0:
+        print("No hay tareas guardadas para modificar.")
+        return
+    listar_tareas()
+
+    try:
+        opcion_id = input("\nIngrese el número de la tarea que desea completar: ").strip()
+        indice = int(opcion_id) - 1 
+        if indice < 0 or indice >= len(lista_tareas):
+            print("Error: El número de tarea no existe en el sistema.")
+            return
+        
+        lista_tareas[indice]["estado"] = "Completada"
+        print(f" Genial! La tarea '{lista_tareas[indice]['nombre']}' ahora está Completada.")
+        
+    except ValueError:
+        print("Error: Debe ingresar un número entero positivo válido.")
 
 # mientras hacia el programa descubri que para hacer un bucle mas basico con funciones
 # usar continuar y agregar True y false es mas practico como ahora
 def iniciar_programa():
     continuar = True
     
-    #como continuar es true llamara a la funcion que mostrara el menu y luego pedira al usuario que ingrese una opcion, dependiendo de la opcion se llamara a la funcion correspondiente, si el usuario ingresa 5 se cambiara continuar a False y el programa terminara, si el usuario ingresa una opcion no valida se mostrara un mensaje de error y el menu se mostrara nuevamente
+    #como continuar es true llamara a la funcion que mostrara el menu y luego pedira al usuario que ingrese una opcion
     while continuar:
         mostrar_menu()
         opcion = input("Seleccione una opción (1-5): ")
         
         # hasta ahora dejare la estructura del menu, al siguiente dia actualizare el menu semi funcional :p
-        #miercoles 17, implemente la opcion 1 y 2 funcionales 
-
+        # miercoles 17, implemente la opcion 1 y 2 funcionales 
+        # Jueves 18: implementamos la opcion 3 jeje
         if opcion == "1":
             agregar_tarea()
         elif opcion == "2":
             listar_tareas()
         elif opcion == "3":
-            print("\n[en desarrollo] Aquí se marcarán las tareas como completadas")
+            completar_tarea()
         elif opcion == "4":
             print("\n[en desarrollo] Aquí se eliminarán las tareas")
         elif opcion == "5":
@@ -65,6 +87,5 @@ def iniciar_programa():
         else:
             print("\n Opción no válida. Por favor, ingrese un número entero positivo del 1 al 5")
 
-# Bloque principal para ejecutar el programa
 if __name__ == "__main__":
     iniciar_programa()
